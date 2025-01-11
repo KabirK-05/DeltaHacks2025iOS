fe5b8c68d0d8a43750c46c17a2508de8e923efab
Karl-Michaud <karl.michaud@mail.utoronto.ca>
2025-01-11 15:39:40 -0500

    Completion of the text box used to prompt the chat bot

---
+++ DeltaHacks/DeltaHacks/ChatBot/Views/ChatBotIntro.swift
@@ -0,0 +1,18 @@
+//
+//  ChatBotIntro.swift
+//  DeltaHacks
+//
+//  Created by Karl-Alexandre Michaud on 2025-01-11.
+//
+
+import SwiftUI
+
+struct ChatBotIntro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatBotIntro()
+}
---
+++ DeltaHacks/DeltaHacks/ChatBot/Views/TextBox.swift
@@ -0,0 +1,37 @@
+//
+//  TextBox.swift
+//  DeltaHacks
+//
+//  Created by Karl-Alexandre Michaud on 2025-01-11.
+//
+
+import SwiftUI
+
+struct TextBox: View {
+    // Variables
+    @State var prompt = ""
+    let characterLimit = 200
+    
+    var body: some View {
+        // Text field creation
+        TextField("Enter Recycling Questions!", text: $prompt, axis: .vertical)
+            .padding()
+//            .frame(height: 175) // Adjust height for 4 lines
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke()
+            )
+            .padding(.horizontal)
+            .onChange(of: prompt) { newValue in
+                // Ensure the character limit is respected
+                if newValue.count > characterLimit {
+                    prompt = String(newValue.prefix(characterLimit))
+                }
+        }
+        
+    }
+}
+    
+#Preview {
+    TextBox()
+}
